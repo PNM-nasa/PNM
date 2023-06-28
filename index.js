5e040b76e37ad9acb4c9d7a4ee9f052cac88b7e5
nasa-code-web <maingocphong177@gmail.com>
2023-06-28 20:06:33 +0700

    edit end

--- index.js
+++ index.js
@@ -13,11 +13,3 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
    console.log("Vào ip localhost:3000")
 });
-/*
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-MongoClient.connect(url, function(err, db) {
-   if (err) throw err;
-   console.log("Database created!");
-   db.close(); }
-);*/
